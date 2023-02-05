bd8f99fc1c8095546c1e4073da4537b543c3c430
yurisoba <tgtay@posteo.jp>
2023-02-05 17:36:35 +0800

    Refactor Parse function
    Use a single Parse function for easy access to global dictionaries.

--- internal/gdoc/elements.go
+++ internal/gdoc/elements.go
@@ -1,7 +1,5 @@
 package gdoc
 
-import "strings"
-
 /*
 TODOs:
 1. Bullet
@@ -75,73 +73,6 @@ func (tr *TextRun) exists() bool {
 	return len(tr.Content) > 0
 }
 
-func (tr *TextRun) toHTML() string {
-	result := tr.Content
-	result = strings.Replace(result, "\n", "<br />", -1)
-	result = strings.Replace(result, "\u000b", "<br />", -1)
-	if tr.TextStyle.Bold {
-		result = "<b>" + result + "</b>"
-	}
-	if tr.TextStyle.Italic {
-		result = "<i>" + result + "</i>"
-	}
-	if tr.TextStyle.Underline {
-		result = "<u>" + result + "</u>"
-	}
-	return "<span>" + result + "</span>"
-}
-
-func (pe *ParagraphElement) setType() {
-	if pe.TextRun.exists() {
-		pe.Type = "TextRun"
-	} else {
-		pe.Type = "Unknown"
-	}
-}
-
-func (pe *ParagraphElement) toHTML() string {
-	switch pe.Type {
-	case "TextRun":
-		return pe.TextRun.toHTML()
-	}
-	return ""
-}
-
 func (paragraph *Paragraph) exists() bool {
 	return len(paragraph.Elements) > 0
-}
-
-func (paragraph *Paragraph) toHTML() string {
-	result := ""
-	for _, elem := range paragraph.Elements {
-		result += elem.toHTML()
-	}
-	switch paragraph.ParagraphStyle.NamedStyleType {
-	case "HEADING_1":
-		return "<h1>" + result + "</h1>"
-	case "HEADING_2":
-		return "<h2>" + result + "</h2>"
-	case "HEADING_3":
-		return "<h3>" + result + "</h3>"
-	}
-	return "<p>" + result + "</p>"
-}
-
-func (se *StructuralElement) setType() {
-	if se.Paragraph.exists() {
-		se.Type = "Paragraph"
-		for i, _ := range se.Paragraph.Elements {
-			se.Paragraph.Elements[i].setType()
-		}
-	} else {
-		se.Type = "Unknown"
-	}
-}
-
-func (se *StructuralElement) toHTML() string {
-	switch se.Type {
-	case "Paragraph":
-		return se.Paragraph.toHTML()
-	}
-	return ""
 }
--- internal/gdoc/gdoc.go
+++ internal/gdoc/gdoc.go
@@ -3,6 +3,7 @@ package gdoc
 import (
 	"encoding/json"
 	"log"
+	"strings"
 )
 
 func Parse(data []byte) string {
@@ -14,9 +15,37 @@ func Parse(data []byte) string {
 
 	result := ""
 	for _, se := range doc.Body.Content {
-		se.setType()
-		result += se.toHTML()
-		// se.print()
+		if se.Paragraph.exists() {
+			run := ""
+			for _, elem := range se.Paragraph.Elements {
+				if elem.TextRun.exists() {
+					text := elem.TextRun.Content
+					text = strings.Replace(text, "\n", "<br />", -1)
+					text = strings.Replace(text, "\u000b", "<br />", -1)
+					if elem.TextRun.TextStyle.Bold {
+						text = "<b>" + text + "</b>"
+					}
+					if elem.TextRun.TextStyle.Italic {
+						text = "<i>" + text + "</i>"
+					}
+					if elem.TextRun.TextStyle.Underline {
+						text = "<u>" + text + "</u>"
+					}
+					run += "<span>" + text + "</span>"
+				}
+			}
+			switch se.Paragraph.ParagraphStyle.NamedStyleType {
+			case "HEADING_1":
+				result += "<h1>" + run + "</h1>"
+			case "HEADING_2":
+				result += "<h2>" + run + "</h2>"
+			case "HEADING_3":
+				result += "<h3>" + run + "</h3>"
+			default:
+				result += "<p>" + run + "</p>"
+			}
+		}
 	}
+
 	return "<html><body>" + result + "</body></html>"
 }
