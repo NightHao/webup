a78df7942f1aea29ae4d4b8a73b6e0f7ed02b798
yurisoba <tgtay@posteo.jp>
2023-04-30 23:12:13 +0800

    Support nested menu

--- internal/cms/menu.go
+++ internal/cms/menu.go
@@ -5,19 +5,56 @@ import (
 	"errors"
 	"log"
 	"strconv"
+
 	"webup/internal/gsheet"
 )
 
 type MenuItem struct {
-	Label   string `json:"label"`
-	Link    string `json:"link"`
-	Lang    string `json:"lang"`
-	Id      string `json:"id"`
-	driveId string
+	Title    string     `json:"title"`
+	Link     string     `json:"link"`
+	Data     string     `json:"data"`
+	Type     string     `json:"type"`
+	Lang     string     `json:"lang"`
+	Children []MenuItem `json:"children"`
+	driveId  string
+}
+
+func parseItem(vals []gsheet.Value,
+	startIdx, length int) (int, []MenuItem) {
+	// TITLE | LINK | DATA | TYPE | LANG | LEVEL
+	//   0   |  1   |  2   |  3   |  4   |  5
+	arr := make([]MenuItem, 0)
+	for i := startIdx; i < length; {
+		item := vals[i]
+		mi := MenuItem{
+			Title:    item[0],
+			Link:     item[1],
+			Data:     strconv.Itoa(i), // index of the menu entry
+			Type:     item[3],
+			Lang:     item[4],
+			Children: nil,
+			driveId:  "",
+		}
+		i++
+		switch item[5] {
+		case "<":
+			// last item of this level
+			arr = append(arr, mi)
+			return i, arr
+		case ">":
+			// nest
+			i, mi.Children = parseItem(vals, i, length)
+			arr = append(arr, mi)
+			break
+		default:
+			arr = append(arr, mi)
+		}
+	}
+	return length, arr
 }
 
 func GetMenu(id string) ([]MenuItem, error) {
-	raw, err := gsheet.Request(id, "A1:D100")
+	raw, err := gsheet.Request(id, "A2:F100")
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -27,37 +64,56 @@ func GetMenu(id string) ([]MenuItem, error) {
 		return []MenuItem{}, err
 	}
 
-	items := make([]MenuItem, len(val.Values))
+	_, items := parseItem(val.Values, 0, len(val.Values))
+	return items, nil
+}
 
-	for i, item := range val.Values {
-		items[i] = MenuItem{
-			Label:   item[0],
-			Link:    item[1],
-			Lang:    item[3],
-			Id:      strconv.Itoa(i),
-			driveId: item[2],
+func findFromNested(vals []gsheet.Value,
+	startIdx, targetIdx, length int) (int, string) {
+	for i := startIdx; i < length; {
+		item := vals[i]
+		if i == targetIdx {
+			return i, item[2]
+		}
+		i++
+		switch item[5] {
+		case "<":
+			return i, ""
+		case ">":
+			if ni, r := findFromNested(vals, i, targetIdx, length); r != "" {
+				return ni, r
+			} else {
+				i = ni
+			}
+			break
 		}
 	}
-	return items, nil
+	return length, ""
 }
 
 func ResolveDriveId(cmsBase, safeId string) (string, error) {
 	var err error
-	var menu []MenuItem
 	var intId int
-
-	menu, err = GetMenu(cmsBase)
-	if err != nil {
-		return "", err
-	}
 
 	intId, err = strconv.Atoi(safeId)
 	if err != nil {
 		return "", err
 	}
 
-	if intId >= len(menu) {
+	raw, err := gsheet.Request(cmsBase, "A2:F100")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	val := gsheet.ValueRange{}
+	err = json.Unmarshal(raw, &val)
+	if err != nil {
+		return "", err
+	}
+
+	_, driveId := findFromNested(val.Values, 0, intId, len(val.Values))
+	if driveId == "" {
 		return "", errors.New("out of range")
 	}
-	return menu[intId].driveId, nil
+
+	return driveId, nil
 }
